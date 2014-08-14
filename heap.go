86c957b375645823dbd40f22b695f0f389e1e3b8
David Huie <david@nationbuilder.com>
2014-08-14 14:22:40 -0700

    Untested implementation

---
+++ heap.go
@@ -0,0 +1,102 @@
+package heap
+
+import (
+	"errors"
+)
+
+type Interface interface {
+	Less(Interface) bool
+}
+
+type Heap struct {
+	data []Interface
+}
+
+func NewHeap() *Heap {
+	return &Heap{
+		make([]Interface, 0),
+	}
+}
+
+func parentIndex(i int) (int, error) {
+	if i <= 0 {
+		return 0, errors.New("invalid index")
+	}
+	return (i - 1) / 2, nil
+}
+
+func childIndicies(i int) (int, int) {
+	return 2*i + 1, 2*i + 2
+}
+
+func (h *Heap) swap(i, j int) {
+	a := h.data[i]
+	h.data[i] = h.data[j]
+	h.data[j] = a
+}
+
+func (h *Heap) Insert(e Interface) {
+	h.data = append(h.data, e)
+	index := len(h.data) - 1
+
+	// Move element from base to correct positioning.
+	for {
+		parentI, err := parentIndex(index)
+		if err != nil {
+			return
+		}
+
+		// Swap with parent if necessary.
+		if e.Less(h.data[parentI]) {
+			break
+		} else {
+			h.swap(index, parentI)
+			index = parentI
+			e = h.data[parentI]
+		}
+	}
+}
+
+func (h *Heap) Delete() Interface {
+	value := h.data[0]
+
+	// Truncate the data and make the last element the root.
+	dataLen := len(h.data)
+	newRoot := h.data[dataLen-1]
+	h.data[0] = newRoot
+	h.data = h.data[:dataLen-2]
+	dataLen = dataLen - 1
+
+	index := 0
+	var swap int
+
+	// Trickle down root to correct position.
+	for {
+		c1, c2 := childIndicies(index)
+
+		// Find correct child to attempt swap with.
+		if c1 > dataLen && c2 > dataLen {
+			break
+		} else if c1 > dataLen {
+			swap = c2
+		} else if c2 > dataLen {
+			swap = c1
+		} else {
+			if h.data[c1].Less(h.data[c2]) {
+				swap = c2
+			} else {
+				swap = c1
+			}
+		}
+
+		// Swap if necessary.
+		if h.data[index].Less(h.data[swap]) {
+			h.swap(index, swap)
+			index = swap
+		} else {
+			break
+		}
+	}
+
+	return value
+}
